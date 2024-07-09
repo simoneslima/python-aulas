d764fd43bdb2f01e7a3c1900384066bc9295042e
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-07-09 17:02:13 -0300

    cores basicas no python

--- desafio34.py
+++ desafio34.py
@@ -3,4 +3,4 @@ if salario <= 1250:
     novo = salario  + (salario * 15 / 100)
 else:
     novo = salario + (salario * 10 / 100)
-print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salario, novo))  
+print('Quem ganhava R${}{:.2f}{} passa a ganhar R${}{:.2f}{} agora.'.format('\033[7;31m',salario,'\033[m','\033[7;32;42m', novo,'\033[m' ))  
