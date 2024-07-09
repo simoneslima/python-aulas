4a04be02bb3e0d1499ed17242a5325e7eadff63d
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-07-09 16:38:36 -0300

    coloquei uma cor

--- desafio35.py
+++ desafio35.py
@@ -1,10 +1,11 @@
 print('-=' * 20)
-print('Analizador de Triangulos')
+#Coloquei uma cor 
+print('\033[4;30;45mAnalizador de Triangulos\033[m')
 print('-=' * 20)
 angulo1 = float(input('Primeiro Angulo: '))
 angulo2 = float(input('Segundo Angulo: '))
 angulo3 = float(input('Terceiro Angulo: '))
 if angulo1 < angulo2 + angulo3 and angulo2 < angulo1 + angulo3 and angulo3 < angulo1 + angulo2:
-    print('Os Angulos acima podem formar um triângulo! ')
+    print('\033[7;33;40mOs Angulos acima podem formar um triângulo!\033[m ')
 else:
-    print('Os Angulos acima não podem formar um triâgulo! ')
+    print('\033[7;37;41mOs Angulos acima não podem formar um triâgulo!\033[m')
