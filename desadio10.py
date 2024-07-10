08b40c81055f993c805a4e3576dd321d41e7b28f
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-07-10 19:15:47 -0300

    treinando cores

--- desadio10.py
+++ desadio10.py
@@ -1,4 +1,4 @@
 real = float(input('Quantos reais vc possui para converter-los?'))
 dollar = real / 4.97
 euro = real / 5.40
-print('Com R$ {:.2f} Reais, você pode comprar USD {:.2f} em Dollares ou EUR {:.2f} em Euros.'.format(real, dollar, euro))
+print('Com R$ {}{:.2f}{} Reais, você pode comprar USD {}{:.2f}{} em Dollares ou EUR {}{:.2f}{} em Euros.'.format('\033[7;31m',real,'\033[m','\033[7;31m', dollar,'\033[m','\033[7;31m', euro,'\033[m'))
