d89f300240ca1bc4b5cfa74f744260c3e9bd5ab1
Simone S. Lima <97068922+simoneslima@users.noreply.github.com>
2024-07-11 18:15:19 -0300

    treinando cores

--- desafio01.py
+++ desafio01.py
@@ -1,4 +1,4 @@
-nome = input('Qual é o seu nome?')
-print ('Ola,', nome, '! Prazer em te conhecer!')
-banda = input('Qual sua banda favorita?')
-print ('Muito legal!', banda)
+nome = input('\033[7;;31mQual é o seu nome?\033[m')
+print ('\033[7;;33mOla,\033[m','\033[7;;32m',nome,'!','\033[m','\033[7;;36mPrazer em te conhecer!\033[m')
+banda = input('\033[7;;31mQual sua banda favorita?\033[m')
+print ('\033[7;;33mTem muito muito bom gosto!\033[m','\033[7;;32m',nome,'\033[7;;32m,\033[m','\033[7;;32m',banda,'\033[m','\033[m''\033[7;;33mé uma otima banda!\033[m')
